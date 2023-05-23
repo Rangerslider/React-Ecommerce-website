5916ca860cebaefe2ed8d1cdbd37146f55e071c1
nuri <nuri1999sept@gmail.com>
2023-05-23 15:24:46 +0600

    adhook changes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import Home from "../components/MainPage/Home"
 import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
-import NewArrivals from "../components/newarrivals/NewArrivals"
 import Discount from "../components/discount/Discount"
 import Shop from "../components/shops/Shop"
 import Annocument from "../components/annocument/Annocumennt"
 import Wrapper from "../components/wrapper/Wrapper"
+
 
 const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
   return (
@@ -14,7 +14,6 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
       <Home CartItem={CartItem} />
       <FlashDeals productItems={productItems} addToCart={addToCart} />
       <TopCate />
-      <NewArrivals />
       <Discount />
       <Shop shopItems={shopItems} addToCart={addToCart} />
       <Annocument />
